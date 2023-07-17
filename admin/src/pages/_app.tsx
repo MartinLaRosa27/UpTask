5f912fb863e4be9b1878202a0a90852bfc9c11e0
martinlarosa27 <martinlarosa27@gmail.com>
2023-07-17 16:30:42 -0300

    ultimos cambios

--- admin/src/pages/_app.tsx
+++ admin/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -7,6 +8,10 @@ import { CategoryContext } from "@/context/CategoryContext";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>UpTask2023 - Admin</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <CategoryContext>
         <Toaster />
         <Component {...pageProps} />
